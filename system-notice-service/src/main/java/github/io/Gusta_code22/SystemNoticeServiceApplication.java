c8642e99f09c10388149c1518ad0a128617507cd
Gusta-code22 <gustavobritomirandaxbox@gmail.com>
2025-12-28 08:33:56 -0300

    Criaçao do primeiro arquivo do projeto

---
+++ system-notice-service/src/main/java/github/io/Gusta_code22/SystemNoticeServiceApplication.java
@@ -0,0 +1,13 @@
+package github.io.Gusta_code22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemNoticeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemNoticeServiceApplication.class, args);
+	}
+
+}
---
+++ system-notice-service/src/test/java/github/io/Gusta_code22/SystemNoticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.Gusta_code22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemNoticeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
